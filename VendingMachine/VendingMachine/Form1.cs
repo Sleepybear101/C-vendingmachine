0aaff49e03720d00d3ed7570025788ec1d0ec140
wurud salih <wu.rud@hotmail.com>
2019-04-25 14:31:59 +0200

    Update Form1.cs

--- VendingMachine/VendingMachine/Form1.cs
+++ VendingMachine/VendingMachine/Form1.cs
@@ -8,25 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+
 namespace VendingMachine
 {
     public partial class Form1 : Form
     {
-        public SqlDbConnection con;
+
+        public Form1()
+        {
+          InitializeComponent();
+         pnlProduct.Controls.Clear();
+         //GetProduct Allespoduct = new GetProduct();
+        // Allespoduct.AlleProduct();
+         Getinfo();
+        }
+       SqlDbConnection con;
+        public string Nummer;
+        public string Prijs;
+        public string id;
+        public string Aantal;
+        public Image Img1;
+
         Image returnImage;
         List<uProduct> Productenlist;
         List<string> idProducten;
-        string name;
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        public DataRow dr;
 
-        public void SetProduct()
-        {
-            con = new SqlDbConnection();
-            flowLayoutPanel1.Controls.Clear();
 
+        public void Getinfo()
+        {
+            SqlDbConnection con = new SqlDbConnection();
             Productenlist = new List<uProduct>();
             idProducten = new List<string>();
             con.SqlQuery("SELECT * FROM `producten` INNER JOIN `systeem` ON producten.id_product=systeem.Product");
@@ -35,41 +46,38 @@ namespace VendingMachine
                 uProduct proitem = new uProduct(this);
 
                 byte[] myImage = (byte[])dr[5];
-                
 
-                string id = dr[1].ToString();
-                string Prijs = dr[2].ToString();
-                string Nummer = dr[8].ToString();
-                string Aantal = dr[4].ToString();
+
+                id = dr[1].ToString();
+                Prijs = dr[2].ToString();
+                Nummer = dr[8].ToString();
+                Aantal = dr[4].ToString();
 
                 proitem.ProductPrijs = Prijs;
                 proitem.ProductNummer = Nummer;
                 proitem.ProductAantal = Aantal;
-                Image img1 = byteArrayToImage(myImage);
-                proitem.afbeelding = img1;
-                //pictureBox1.Image = new Bitmap(new MemoryStream(myImage));
+                Img1 = byteArrayToImage(myImage);
+                proitem.afbeelding = Img1;
                 Productenlist.Add(proitem);
-                idProducten.Add(id);
-          
-            }
-                flowLayoutPanel1.Controls.AddRange(Productenlist.ToArray());
-        }
-        public Image byteArrayToImage(byte[] byteArrayIn)
-        {
-            try
-            {
-                MemoryStream ms = new MemoryStream(byteArrayIn, 0, byteArrayIn.Length);
-                ms.Write(byteArrayIn, 0, byteArrayIn.Length);
-                returnImage = Image.FromStream(ms, true);//Exception occurs here
-            }
-            catch { }
-            return returnImage;
-        }
-        private void Form1_Load_1(object sender, EventArgs e)
-        {
-            SetProduct();
-        }
 
+                idProducten.Add(id);
+            }
+            pnlProduct.Controls.AddRange(Productenlist.ToArray());
+        }
+            public Image byteArrayToImage(byte[] byteArrayIn)
+            {
+                try
+                {
+                    MemoryStream ms = new MemoryStream(byteArrayIn, 0, byteArrayIn.Length);
+                    ms.Write(byteArrayIn, 0, byteArrayIn.Length);
+                    returnImage = Image.FromStream(ms, true); //Exception occurs here
+                }
+                catch
+                {
+                }
+
+                return returnImage;
+            }
         private void button20_Click(object sender, EventArgs e)
         {
             BeheerTool Beheerder = new BeheerTool();
