a22d624e8a5972465395afc0d60f112649248ceb
wurud salih <wu.rud@hotmail.com>
2019-04-25 14:31:34 +0200

    Update BeheerTool.cs

--- VendingMachine/VendingMachine/BeheerTool.cs
+++ VendingMachine/VendingMachine/BeheerTool.cs
@@ -7,44 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace VendingMachine
 {
     public partial class BeheerTool : Form
     {
+
         public SqlDbConnection con;
 
         public BeheerTool()
         {
             InitializeComponent();
-            GetInfo();
+            mySqlConnection();
         }
 
-        private void BeheerTool_Load(object sender, EventArgs e)
+        private void mySqlConnection()
         {
 
+            con = new SqlDbConnection();
+            con.SqlQuery("SELECT * FROM `producten`");
+            dataGridView1.DataSource = con.QueryEx();
+
+
         }
 
-        DataTable table = new DataTable();
-    
-
-        private void GetInfo()
+        private void SearchTextBox_TextChanged_1(object sender, EventArgs e)
         {
-          con = new SqlDbConnection();
-          con.SqlQuery("SELECT `id_product`,`Naam`,`Prijs`,`AantalGekocht`,`Voorraad`  FROM snoepmachine.producten"));
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Format(" Naam LIKE '{0}%'", searchTextBox.Text);
 
-            DataSet ds = new DataSet();
-          
-            dataGridView1.DataSource = ds.Tables["producten"];
-         
-
-                dataGridView1.DataSource = ds.Tables["producten"];
-
-
-            }
-
-    */
         }
 
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            con = new SqlDbConnection();
+            string NieuwVoorraad = textBoxVoorraad.Text;
+            string Product = searchTextBox.Text;
+            con.SqlQuery("UPDATE `producten` SET `Voorraad`=@Voorraad WHERE `Naam` =@produc");
+            con.Cmd.Parameters.Add("@Voorraad", NieuwVoorraad);
+            con.Cmd.Parameters.Add("@product", Product);
+            con.NonQueryEx();
+            mySqlConnection();
+
+        }
     }
 }
